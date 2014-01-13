513145bb5537d3343087c72812a1c53bf8409a36
Benjamin Landers <benjaminrlanders@gmail.com>
2014-01-12 19:16:11 -0800

    added some tests

--- src/test.js
+++ src/test.js
@@ -1,8 +1,12 @@
-function test(output,input){
-  if(output.join(",")!==clean(input).join(','))
+function test(func,output,input){
+  if(output.join(",")!==func(input).join(','))
     throw output + ", "+ input;
   return true;
 }
-test(["a","b","c","d","e","f"],"a b c d e f");
-test(["a","b","c","'d e'","f","g"],"a b c 'd e' f g");
+test(clean,["a","b","c","d","e","f"],"a b c d e f");         //basic
+test(clean,["a","b","c","'d e'","f","g"],"a b c 'd e' f g"); //quotes
+test(clean,["a","b","c","f","g"],"a b c ;d e\n f g");        //single comment
+test(clean,["a","b","c","f","g"],"a b c #|d \n e|# f g");    //block comment
+test(clean,["a","b","c","f","g"],"a b c #|d  'e|# f g");     //quote in block
+test(clean,["a","b","c","'d;e'","f","g"],"a b c 'd;e' f g"); //comment in string
 console.log('Tests complete');
