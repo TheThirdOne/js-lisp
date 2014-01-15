709e56f18f5ab89272ed9b1ca8db2c97ff31867b
Benjamin Landers <benjaminrlanders@gmail.com>
2014-01-14 18:06:29 -0800

    implemented a stdlib

---
+++ src/stdlib.js
@@ -0,0 +1,43 @@
+env.do = function(args){
+  var c;
+  for(var i = 0; i < args.length;i++){ 
+    c = args[i]();
+  }
+  return c;
+};
+env.log = function(args){
+  var str = "";
+  for(var i = 0; i < args.length;i++){
+    str += args[i]();
+  }
+  console.log(str);
+  return str;
+};
+env['+'] = function(args){
+  var out = args[0]();
+  for(var i = 1; i < args.length;i++){
+    out += args[i]();
+  }
+  return out;
+};
+env['*'] = function(args){
+  var out = args[0]();
+  for(var i = 1; i < args.length;i++){
+    out *= args[i]();
+  }
+  return out;
+};
+env['-'] = function(args){
+  var out = args[0]();
+  for(var i = 1; i < args.length;i++){
+    out -= args[i]();
+  }
+  return out;
+};
+env['/'] = function(args){
+  var out = args[0]();
+  for(var i = 1; i < args.length;i++){
+    out /= args[i]();
+  }
+  return out;
+};
